a1643de49df82ab30956157e35124f50e9b9ff77
Bennett Lawson <bclaw@umich.edu>
2020-12-24 15:50:56 -0500

    more error checking - for promotions

--- chess_main.py
+++ chess_main.py
@@ -4,15 +4,14 @@ import chess
 from chess_engine import ChessEngine
 from engines.human_engine import HumanEngine
 from engines.random_engine import RandomEngine
+#from simple_engine import SimpleEngine
 
 
 def main():
-
-    white_engine = RandomEngine(chess.WHITE)
+    white_engine = HumanEngine(chess.WHITE)
     black_engine = RandomEngine(chess.BLACK)
 
     c = ChessEngine(white_engine, black_engine)
-
     c.run()
 
 
--- engines/human_engine.py
+++ engines/human_engine.py
@@ -2,29 +2,6 @@ from engines.abstract_engine import AbstractEngine
 from chess import Board, Move
 
 
-# Example implementation
-def check_string(string_move):
-    if len(string_move) < 4 or len(string_move) > 5:
-        return False
-
-    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    ints = [1, 2, 3, 4, 5, 6, 7, 8]
-
-    if string_move[0] not in chars or string_move[2] not in chars:
-        return False
-    if string_move[1] not in ints or string_move[3] not in ints:
-        return False
-
-    if len(string_move == 4):
-        return True
-
-    pieces = ['q', 'r', 'b', 'n']
-    if string_move[4] not in pieces:
-        return False
-    else:
-        return True
-
-
 class HumanEngine(AbstractEngine):
     def __init__(self, color):
         super().__init__(color)
@@ -35,12 +12,13 @@ class HumanEngine(AbstractEngine):
         while True:
             string_move = input("Enter move:").lower()
 
-            if check_string(string_move):
-                print("\nWrong string length - illegal move\n")
-                continue
+            # if not check_string(string_move):
+            #     print("\nWrong string length - illegal move\n")
+            #     continue
 
-            move = Move.from_uci(string_move)
-            if move in legal_moves:
-                return move
-
-            print("\nIllegal move. Example move: a1b1\n")
+            try:
+                move = Move.from_uci(string_move)
+                if move in legal_moves:
+                    return move
+            except:
+                print("\nIllegal move. Example move: a1b1\n")
