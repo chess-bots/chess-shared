8953020a31060f7f61c1e4048397c166f450ad50
Justin <jnewberry0502@gmail.com>
2020-12-24 19:38:07 +0000

    added game status messages

--- chess_engine.py
+++ chess_engine.py
@@ -20,5 +20,10 @@ class ChessEngine:
             self.board.push(move)
 
             if self.board.is_game_over():
-                print("game is over")
+                if self.board.result() == "1/2-1/2":
+                    print("Game ended in draw")
+                if self.board.result() == "1-0":
+                    print("White won the game")
+                if self.board.result() == "0-1":
+                    print("Black won the game")
                 break
