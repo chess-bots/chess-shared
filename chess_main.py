463a07c1150e428b336500a45afcaa5f7b3f62eb
Bennett Lawson <bclaw@umich.edu>
2020-12-26 12:22:34 -0500

    Added variables to point to stockfish on the correct machines

--- chess_main.py
+++ chess_main.py
@@ -5,13 +5,14 @@ from chess_engine import ChessEngine
 from engines.human_engine import HumanEngine
 from engines.random_engine import RandomEngine
 from simple_engine import SimpleEngine
+from engines.stockfish_engine import StockfishEngine, STOCKFISH_BENNETT_PATH, STOCKFISH_JUSTIN_PATH
 
 
 def main():
-    white_engine = SimpleEngine(chess.WHITE, depth=3)
-    black_engine = SimpleEngine(chess.BLACK, depth=2)
+    white_engine = SimpleEngine(chess.WHITE, depth=1)
+    black_engine = StockfishEngine(False, 1.0, path=STOCKFISH_BENNETT_PATH)
 
-    c = ChessEngine(white_engine, black_engine, svg_graphics=True, min_display_time=2.0)
+    c = ChessEngine(white_engine, black_engine, svg_graphics=True, min_display_time=-1)
     c.run()
 
 
--- engines/stockfish_engine.py
+++ engines/stockfish_engine.py
@@ -1,12 +1,16 @@
 import chess
-
+import chess.engine as chess_engine
 from engines.abstract_engine import AbstractEngine
 
+STOCKFISH_JUSTIN_PATH = "/var/run/stockfish/stockfish_10_x64"
+STOCKFISH_BENNETT_PATH = "C:\Code\Personal\Chess\stockfish\stockfish_12.exe"
+
+
 class StockfishEngine(AbstractEngine):
-    def __init__(self, color, think_time = 5.0):
+    def __init__(self, color, think_time=5.0, path=STOCKFISH_JUSTIN_PATH):
         super().__init__(color)
-        self.engine = chess.engine.SimpleEngine.popen_uci("/var/run/stockfish/stockfish_10_x64")
+        self.engine = chess_engine.SimpleEngine.popen_uci(path)
         self.think_time = think_time
-        
+
     def get_move(self, board: chess.Board):
         return self.engine.play(board, limit=chess.engine.Limit(self.think_time)).move
