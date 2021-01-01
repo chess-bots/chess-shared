0dbdfcf4b732f1d7c938b579e3a9c18bd82a186f
Justin <jnewberry0502@gmail.com>
2021-01-01 02:52:55 +0000

    comments

--- engines/abstract_engine.py
+++ engines/abstract_engine.py
@@ -10,6 +10,10 @@ class AbstractEngine:
     def get_move(self, current_board: chess.Board) -> chess.Move:
         print("ERROR: Function not overriden in child class")
     
-    # since the automated bots won't be able to handle an invalid move, just raise an exception
+    """
+    This is mostly for human bots to override and tell them they made an invalid move
+    since the automated bots won't be able to handle an invalid move, just raise an exception by default
+    If you think of a way for your bot to handle this, override this function
+    """
     def on_invalid_move(self, board, move):
         raise Exception("Your engine made an invalid move... exiting", move)
