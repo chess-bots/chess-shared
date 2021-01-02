db77e4696ce470db8da31ad6f6ff82089ecb0a16
Justin <jnewberry0502@gmail.com>
2021-01-02 01:15:29 +0000

    replaced skill level with elo, which is easier to use

--- engines/stockfish_engine.py
+++ engines/stockfish_engine.py
@@ -2,7 +2,7 @@ import chess
 import chess.engine as chess_engine
 from engines.abstract_engine import AbstractEngine
 
-STOCKFISH_JUSTIN_PATH = "/var/run/stockfish/stockfish_10_x64"
+STOCKFISH_JUSTIN_PATH = "/var/run/stockfish/stockfish_12"
 STOCKFISH_BENNETT_PATH = "C:\Code\Personal\Chess\stockfish\stockfish_12.exe"
 
 
@@ -10,13 +10,14 @@ class StockfishEngine(AbstractEngine):
     """
     Skill level is 1-7
     """
-    def __init__(self, color, think_time=5.0, skill_level=1, path=STOCKFISH_JUSTIN_PATH):
+    def __init__(self, color, think_time = 10, elo = None, path=STOCKFISH_JUSTIN_PATH):
         super().__init__(color)
         self.engine = chess_engine.SimpleEngine.popen_uci(path)
         self.think_time = think_time
-        self.skill_level = skill_level
+        self.elo = elo
 
     def get_move(self, board: chess.Board):
         return self.engine.play(board, limit=chess.engine.Limit(self.think_time), options={
-            "Skill Level": self.skill_level
+            "UCI_LimitStrength": True if self.elo is not None else False,
+            "UCI_Elo": self.elo
         }).move
